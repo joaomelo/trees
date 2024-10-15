2b2ddde97b799a1be2041985ca2bb2bc4b43da94
joão melo <j@melo.plus>
2024-10-15 17:58:29 -0300

    chore: removed legacy functions

--- src/filter.js
+++
@@ -1,8 +0,0 @@
-export function filterTree(tree, filter) {
-  const result = [];
-  tree.forEach((node) => {
-    if (filter(node)) return result.push(node);
-    result.push(...filterTree(node.children, filter));
-  });
-  return result;
-}
--- src/flat.js
+++
@@ -1,8 +0,0 @@
-export function flatTree(tree) {
-  const result = [];
-  tree.forEach(({ children, ...data }) => {
-    result.push(data);
-    result.push(...flatTree(children));
-  });
-  return result;
-}
